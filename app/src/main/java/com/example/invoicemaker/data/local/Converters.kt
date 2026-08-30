942583b070b8828cd872e823115abe7c6a1ff779
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-08-30 18:58:37 +0100

    updates to Converters.kt

--- app/src/main/java/com/example/invoicemaker/data/local/Converters.kt
+++ app/src/main/java/com/example/invoicemaker/data/local/Converters.kt
@@ -2,6 +2,7 @@ package com.example.invoicemaker.data.local
 
 import androidx.room.TypeConverter
 import java.math.BigDecimal
+import java.time.LocalDate
 
 class Converters {
     @TypeConverter
@@ -9,4 +10,10 @@ class Converters {
 
     @TypeConverter
     fun toBigDecimal(value: String?): BigDecimal? = value?.let { BigDecimal(it) }
+
+    @TypeConverter
+    fun fromLocalDate(value: LocalDate?): String? = value?.toString() // ISO-8601: "2026-08-30"
+
+    @TypeConverter
+    fun toLocalDate(value: String?): LocalDate? = value?.let { LocalDate.parse(it) }
 }
